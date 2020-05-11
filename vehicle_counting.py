43453d56fad016e3f024ca2ebe58e9311f38256b
Rahmadillah Maulana <rahmadillah.maulana11@gmail.com>
2020-05-11 18:08:53 +0700

    Update vehicle_counting.py

--- vehicle_counting.py
+++ vehicle_counting.py
@@ -19,6 +19,6 @@ detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018
 
 is_color_recognition_enabled = 1 # set it to 1 for enabling the color prediction for the detected objects
 roi = 185 # roi line position
-deviation = 2 # the constant that represents the object counting area
+deviation = 3 # the constant that represents the object counting area
 
 object_counting_api.cumulative_object_counting_y_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, roi, deviation) # counting all the objects
